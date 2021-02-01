74b946f732faee926162ad10132fd5fa8845a5f7
alitechproed <acan.techproed@gmail.com>
2021-02-01 17:02:24 +0300

    first

--- src/_01_Variables/Advanced_1_Assigning.java
+++ src/_01_Variables/Advanced_1_Assigning.java
@@ -1,5 +1,7 @@
 package _01_Variables;
 
 public class Advanced_1_Assigning {
-
+  public static void main(String[] args) {
+	System.out.println("deneme");
+}
 }
--- src/_01_Variables/Office_1_Assigning.java
+++ src/_01_Variables/Office_1_Assigning.java
@@ -1,5 +1,7 @@
 package _01_Variables;
 
 public class Office_1_Assigning {
+	
+	
 
 }
